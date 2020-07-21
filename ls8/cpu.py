0519df201d354f9986a1280ad9e2b504d557ad0d
Gordon Caister <gordoncaister@gmail.com>
2020-07-21 16:37:59 -0500

    first commit finished HLT handler

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,23 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+        self.branch_table = {
+            0b00000001 : "HLT",
+            0b10000010 : "LDI" ,
+            0b01000111 : "PRN" 
+        }
+
+    def ram_read(self,mar):   #Memory Address Register
+        return self.ram[mar]
+
+    def ram_write(self,mar,mdr):  #Memory Data Register
+        self.ram[mar] = mdr
+
+    def HLT(self):
+        return False
 
     def load(self):
         """Load a program into memory."""
@@ -60,6 +76,102 @@ class CPU:
 
         print()
 
+    
+
     def run(self):
         """Run the CPU."""
-        pass
+        running = True
+        count = 1
+        while running:
+            
+            ir = self.ram[self.pc] 
+
+            if ir in self.branch_table and self.branch_table[ir] == "HLT":
+                running = self.HLT()
+            
+            operand_a = self.ram_read(self.pc+1)
+            operand_b = self.ram_read(self.pc+2)
+
+            if (ir & (1 << 7)) >> 7 == 1:
+                self.pc += 2
+            else:
+                self.pc += 1
+
+            
+
+            
+
+        
+        
+'''
+
+## KEY
+# * `iiiiiiii`: 8-bit immediate value
+# * `00000rrr`: Register number
+# * `00000aaa`: Register number
+# * `00000bbb`: Register number
+## Establishing acronyms for all programs to be used in cpu.py
+""" Primary Programs """
+LDI = 0b10000010    ## Load Immediate - store value in register, set register to this value
+LD = 0b10000011     ## Load from Other - Loads RegA with value at the memory address stored in Register B
+PRN = 0b01000111    ## Print numeric value stored in register
+HLT = 0b00000001    ## Halt. Stops CPU and exits emulator
+POP = 0b01000110    ## Pop.
+PUSH = 0b01000101   ## Push.
+NOP = 0b00000000    ## No OPeration. Pass
+CALL = 0b01010000   ## Calls specific subroutine (a function/program) at an address
+# 01010000 00000rrr // This is address. After sending it call(address), the PC moves to the location in RAM and executes that program.
+RET = 0b00010001    ## Return. 
+""" Mathy Programs """
+INC = 0b01100101    ## Increment (add 1) to value of the passed register
+## 01100101 00000rrr
+DEC = 0b01100110    ## Decrement (subtract 1 from) a value in the passed register
+## 01100110 00000rrr // value
+ADD = 0b10100000    ## Add. Add two registers, and replace RegA with the sum
+# 10100000 00000aaa 00000bbb
+SUB = 0b10100001    ## Subtract. Put result in RegA
+MUL = 0b10100010   ## Multiply. Multiplies two Registers together.
+DIV = 0b10100011    ## Divide. RegA/RegB. Result is stored as RegA. Exit case of RegB == 0, then HLT
+## 10100011 00000aaa 00000bbb
+MOD = 0b10100100    ## Remainder. Divide RegA/ReB, place the remainder in RegA. Exit Case of RegB == 0, then HLT
+""" Comparison Programs """
+AND = 0b10101000    ## And. The Bitwise AND ( & ). 
+NOT = 0b01101001    ## Not. The Bitwise-NOT. Uses 1 Register. Stores result in that register
+OR = 0b10101010     ## Or, can be Both. The Bitwise-OR. 2 Registers.
+XOR = 0b10101011    ## One OR the Other, not Both.
+## Comp Graphs
+# A B   AND  OR  XOR  NOR  NAND
+# 0 0    0   0    0    1    1
+# 0 1    0   1    1    0    1
+# 1 0    0   1    1    0    1
+# 1 1    1   1    0    0    0
+CMP = 0b10100111    ## Comparison(rega, regb). Compares two things and returns a value based upon it. Changes specific FL based it. Flag 'E' ==, sets to 1, Flag 'L' RegA < ReB sets to 1, Flag 'G' RegA > RegB sets to 1
+## 10100111 00000aaa 00000bbb, A7 0a 0b
+""" ????????? """
+INT = 0b01010010    ## Interrupt ??????????????????
+      # Issue the interrupt number stored in the given register.
+      # This will set the _n_th bit in the `IS` register to the value in the given
+      # register.
+      # Machine code:
+      # ```
+      # 01010010 00000rrr
+IRET = 0b00010011   ## Return from Interrupt
+""" Jump Statements """
+JMP = 0b01010100    ## Jump to address in the register. Set PC to that address
+# Jump to an address in register if the noted Flags (FL) are set to True (1)
+JNE = 0b10000011    ## Jump if Not Equal
+JEQ = 0b01010101    ## Jump if Equal
+JGE = 0b01011010    ## Jump if Greater/Equal
+JGT = 0b01010111    ## Jump if Greater Than
+JLE = 0b01011001    ## Jump if Less/Equal
+JLT = 0b01011000    ## Jump if Less Than
+""" Other Acronyms """
+## Basic Acronyms
+# CPU = Central Processing Unit
+# ALU = Arithmetic-Logic Unit, carries out maths and logic operations
+## Internal Registers
+# IR = Instruction Register - address of currently running instruction
+# MAR = Memory Address Register - Holds Memory Address we're reading/writing
+# MDR = Memory Data Register - Holds value to write/Value just read
+# FL = Flags (current Flags: E, L, G)
+'''
